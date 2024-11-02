b5ef36571af7e9709ac939d723b77a396769cdc9
Kenjiro Fukuda <kenjirofukuda@gmail.com>
2024-11-02 21:08:59 +0900

    remove unused files

--- FirstTool/obj/FirstTool.obj/NSFileHandle+readLine.m.d
+++
@@ -1,4 +0,0 @@
-obj/FirstTool.obj/NSFileHandle+readLine.m.o: NSFileHandle+readLine.m \
-  NSFileHandle+readLine.h
-
-NSFileHandle+readLine.h:
--- FirstTool/obj/FirstTool.obj/main.m.d
+++
@@ -1,3 +0,0 @@
-obj/FirstTool.obj/main.m.o: main.m NSFileHandle+readLine.h
-
-NSFileHandle+readLine.h:
