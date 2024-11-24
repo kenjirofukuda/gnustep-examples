de1d243083c9d98b26462887398b69fe729a193a
kenjiro fukuda <kenjiro@debian12>
2024-11-24 09:58:03 +0900

    change cursor inside view

--- PointsView/PointsView.h
+++ PointsView/PointsView.h
@@ -8,12 +8,24 @@
 @interface PointsView : NSView
 {
   NSMutableArray *_points;
+#ifndef GNUSTEP  
+  NSTrackingArea *_trackingArea;
+#endif
+  NSTrackingRectTag _trackId;
+
 }
 - (instancetype) initWithFrame: (NSRect) frameRect;
 - (void) dealloc;
 
 - (void) drawRect: (NSRect) area;
 - (void) mouseDown: (NSEvent *) event;
+- (void) mouseEntered: (NSEvent *)event;
+- (void) mouseExited: (NSEvent *)event;
+- (void) viewDidMoveToWindow;
+
+#ifndef GNUSTEP  
+- (void) updateTrackingAreas;
+#endif
 
 @end
 
--- PointsView/PointsView.m
+++ PointsView/PointsView.m
@@ -2,6 +2,11 @@
 
 #import "PointsView.h"
 
+@interface PointsView (Tracking)
+- (void) _frameChanged: (NSNotification *) aNot;
+- (void) _removeTrack;
+@end
+
 @interface PointsView (SimpleDrawing)
 - (NSBezierPath *) circlePathWithCenter: (NSPoint)center radius: (CGFloat)radius;
 @end
@@ -32,13 +37,26 @@
   if (self != nil)
     {
       _points = [[NSMutableArray alloc] init];
+#ifndef GNUSTEP
+      _trackingArea = nil;
+#endif
+      [[NSNotificationCenter defaultCenter] 
+        addObserver: self
+           selector: @selector(_frameChanged:)
+               name: NSViewFrameDidChangeNotification
+             object: self];
+      _trackId = 0;      
     }
   return self;
 }
 
 - (void) dealloc
 {
+  [[NSNotificationCenter defaultCenter] removeObserver: self];
   RELEASE(_points);
+#ifndef GNUSTEP
+  RELEASE(_trackingArea);
+#endif
   DEALLOC;
 }
 
@@ -57,6 +75,14 @@
     }
 }
 
+- (void) viewDidMoveToWindow
+{
+  [super viewDidMoveToWindow];
+  [[NSNotificationCenter defaultCenter] postNotificationName: NSViewFrameDidChangeNotification
+                                                      object: self];
+
+}
+
 - (void) mouseDown: (NSEvent *)event
 {
   NSPoint loc = [self convertPoint: [event locationInWindow] fromView: nil];
@@ -65,5 +91,55 @@
   [self setNeedsDisplay: YES];
 }
 
+- (void) mouseEntered: (NSEvent *)event
+{
+  NSLog(@"mouseEntered: %@", event);
+  [super mouseEntered: event];
+  [[NSCursor crosshairCursor] set];
+}
+
+- (void) mouseExited: (NSEvent *)event
+{
+  NSLog(@"mouseExited: %@", event);
+  [super mouseExited: event];
+  [[NSCursor arrowCursor] set];
+}
+
+#ifndef GNUSTEP  
+- (void) updateTrackingAreas
+{
+  [super updateTrackingAreas];
+  if (_trackingArea != nil)
+    {
+      [self removeTrackingArea: _trackingArea];
+      RELEASE(_trackingAreaa);
+    }
+
+  int opts = (NSTrackingMouseEnteredAndExited | NSTrackingActiveAlways);
+  _trackingArea = [[NSTrackingArea alloc] initWithRect: [self bounds]
+                                               options: opts
+                                                 owner: self
+                                              userInfo: nil];
+  [self addTrackingArea: _trackingArea];
+}
+#endif
+@end
+
+@implementation PointsView (Tracking)
+- (void) _frameChanged: (NSNotification *) aNot
+{
+  [self _removeTrack];
+  _trackId = [self addTrackingRect: [self bounds]
+                             owner: self
+                          userData: NULL
+                      assumeInside: NO];
+}
+
+- (void) _removeTrack
+{
+  if (_trackId)
+    [self removeTrackingRect: _trackId];
+  _trackId = 0;
+}
 
 @end
